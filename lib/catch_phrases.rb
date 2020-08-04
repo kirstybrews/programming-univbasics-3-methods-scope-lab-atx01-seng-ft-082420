f96b68501b65056faac8d19182c02c55d9eb1153
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-04 17:34:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,5 @@
 def mario
   phrase = "It's-a me, Mario!"
-  
   puts phrase
 end
 
@@ -8,3 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  advice = "It's Dangerous To Go Alone! Take This."
+  puts advice
+end 
